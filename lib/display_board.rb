2bd9f1b63b8aef472feaceb873c2b807884ef1bc
Henry Panora <hpanora@gmail.com>
2019-04-15 21:27:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board("tic Tac Toe")
+  puts ("   |   |   ")
+  puts ("-----------")
+  puts ("   |   |   ")
+  puts ("-----------")
+  puts ("   |   |   ")
+
+  
+end
